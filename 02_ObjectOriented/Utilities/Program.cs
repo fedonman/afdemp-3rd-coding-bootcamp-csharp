9f8eb92edd2aa3a7ae596df2169a544e3d593021
Vyron <fedonman@gmail.com>
2017-11-21 16:55:31 +0200

    updates

---
+++ 02_ObjectOriented/Utilities/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. a)
+            int a = 5;
+            int b = 10;
+            Console.WriteLine($"Before -- a: {a}, b: {b}");
+            Utilities.Swap(ref a, ref b);
+            Console.WriteLine($"After -- a: {a}, b: {b}");
+
+            // 1. b)
+            string x = "xxx";
+            string z = "zzz";
+            Console.WriteLine($"Before -- x: {x}, z: {z}");
+            Utilities.Swap(ref x, ref z);
+            Console.WriteLine($"After -- x: {x}, z: {z}");
+
+            // 2. a)
+            List<int> listA = new List<int>() { 1, 5, 5, 3, 4, 4, 4, 4, 6, 6, 6, 6, 6 };
+            List<int> resultA = Utilities.Subsequence(listA);
+
+            Console.Write("Subsequence of integers: ");
+            foreach (int i in resultA)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+
+            // 2. b)
+            List<string> listB = new List<string>() { "tree", "tree", "house", "park", "park", "park", "house", "house" };
+            List<string> resultB = Utilities.Subsequence<string>(listB);
+
+            Console.Write("Subsequence of generics with T=string: ");
+            foreach (string i in resultB)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+
+            List<string> result = listB.GetSubsequence();
+            Console.Write("Subsequence of generics with T=string: ");
+            foreach (string i in result)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_ObjectOriented/Utilities/Utilities.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class Utilities
+    {
+        // 1. a) Swap integers
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // 1. b) Swap generics
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        // 2. a) Maximum Subsequence of integers
+        public static List<int> Subsequence(List<int> list)
+        {
+            int startIndex = 0;
+            int count = 1;
+            int maxCount = 0;
+            int maxStartIndex = 0;
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1] == list[i])
+                {
+                    count++;
+                    if (count > maxCount)
+                    {
+                        maxStartIndex = startIndex;
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        maxStartIndex = startIndex;
+                    }
+                    startIndex = i;
+                    count = 1;
+                }
+            }
+            return list.GetRange(maxStartIndex, maxCount);
+        }
+
+
+        public static List<T> Subsequence<T>(List<T> list)
+        {
+            int startIndex = 0;
+            int count = 1;
+            int maxCount = 0;
+            int maxStartIndex = 0;
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1].Equals(list[i]))
+                {
+                    count++;
+                    if (i == list.Count - 1 && count > maxCount)
+                    {
+                        maxStartIndex = startIndex;
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        maxStartIndex = startIndex;
+                    }
+                    startIndex = i;
+                    count = 1;
+                }
+            }
+            return list.GetRange(maxStartIndex, maxCount);
+        }
+
+        // 2. c) Maximum Subsequence of Generic type as extention method
+        public static List<T> GetSubsequence<T>(this List<T> list)
+        {
+            int startIndex = 0;
+            int count = 1;
+            int maxCount = 0;
+            int maxStartIndex = 0;
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i - 1].Equals(list[i]))
+                {
+                    count++;
+                    if (i == list.Count - 1 && count > maxCount)
+                    {
+                        maxStartIndex = startIndex;
+                        maxCount = count;
+                    }
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        maxStartIndex = startIndex;
+                    }
+                    startIndex = i;
+                    count = 1;
+                }
+            }
+            return list.GetRange(maxStartIndex, maxCount);
+        }
+    }
+}
