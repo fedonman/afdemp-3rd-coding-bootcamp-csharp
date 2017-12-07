24802eb0cf905defd01a5c5fd6b13b555fe5f647
fedonman <fedonman@gmail.com>
2017-12-07 16:55:03 +0200

    adds exercise

--- 03_Frontend/JavaScript/Login/main.js
+++ 03_Frontend/JavaScript/Login/main.js
@@ -1,74 +1,37 @@
-// These functions represent my Model
-function Product(name, price) {
+function Account(name, email, password) {
   this.name = name;
-  this.price = price;
+  this.email = email;
+  this.password = password;
 }
 
-function CartItem(product, quantity = 1) {
-  this.product = product;
-  this.quantity = quantity;
-}
-
-function Cart() {
-  this.items = [];
-  this.calculateTotalCost = function() {
-    let cost = 0;
-    for (let item of this.items) {
-      cost += item.product.price * item.quantity;
-    }
-    return cost;
-  }
-}
-
-// runs after DOM is loaded
-window.onload = function() { 
-  function renderInventory(inventory, inventoryListElement) {
-    inventoryListElement.innerHTML = '';
-    inventory.forEach(x => {
-      inventoryListElement.innerHTML += `<li>${x.name} - ${x.price} <button id="${x.name}">Add</button></li>`;
-    });
-  }
-  
-  function renderCart(cart, cartListElement) {
-    cartListElement.innerHTML = '';
-    cart.items.forEach(x => {
-        cartListElement.innerHTML += `<li>${x.product.name} - ${x.quantity}</li>`;
-    });
-  }
-
-  function renderTotalCost(cost, totalCostElement) {
-    totalCostElement.innerHTML = `Total cost: $${cost}`;
-  }
-
-  /* normally fetch this from server */
-  let inventory = [
-    new Product('Product X', 4.5),
-    new Product('Product Y', 3),
-    new Product('Product Z', 5),
-    new Product('Product K', 6)
+$(document).ready(function() {
+  let accounts = [
+    new Account('Mike', 'mike@gmail.com', '12345'),
+    new Account('Mary', 'mary@gmail.com', '12345')
   ];
-  let cart = new Cart();
 
-  let inventoryListElement = document.getElementById("inventory-list");
-  let cartListElement = document.getElementById("cart-list");
-  let totalCostElement = document.getElementById("total-cost");
+  let loginFormElement = $('#login-form');
+  
+  loginFormElement.submit(function(e) {
+    let email = $('#login-form #email').val();
+    let password = $('#login-form #pwd').val();
 
-  renderInventory(inventory, inventoryListElement);
-
-  let addButtons = inventoryListElement.getElementsByTagName('button');
-  Array.from(addButtons).map(x => {
-    let id = x.getAttribute('id');
-    x.onclick = function(e) {
-      let index = cart.items.findIndex(x => x.product.name === id);
-      if (index === -1) {
-        let product = inventory.find(x => x.name === id);
-        cart.items.push(new CartItem(product, 1));
-      }
-      else {
-        cart.items[index].quantity += 1;
-      }
-      renderCart(cart, cartListElement);
-      renderTotalCost(cart.calculateTotalCost(), totalCostElement);
+    let account = accounts.find(x => x.email === email && x.password === password);
+    if (account === undefined) {
+      let errorElement = $('#login-form #error');
+      errorElement.text('Wrong email/password!');
+      errorElement.show();
     }
+    else {
+      let alertHTML = 
+      `<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Hello ${account.name}!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>`;
+      $('#alerts').append(alertHTML);
+    }
+    e.preventDefault();
   });
-}
+});
